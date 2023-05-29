dc139173ee2e23f14863f6904bbd1c46ae9dfe5d
Dorian7777 <134231624+Dorian7777@users.noreply.github.com>
2023-05-29 13:03:27 +0200

    update 1: pocetak igre

--- Code_Breaker.cpp
+++ Code_Breaker.cpp
@@ -1,7 +1,32 @@
 
 #include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include<fstream>
+
+using namespace std;
+
+
+void igrica(char answer)
+{
+	if (answer == 'n' || answer != 'y')
+		return;
+	
+	cout << "Good, good luck, you'll need it..." << endl;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char answer;
+	string black, white, red, orange, yellow, pink, blue, green, empty;
+	fstream datoteka;
+	cout << "Do you have what it takes to crack the code?" << endl;
+	cout << "press y/n" << endl;
+	cin >> answer;
+	igrica(answer);
+	return 0;
+	
+
 }
+
