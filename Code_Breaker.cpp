275af37cc53ec2f8f4606ca13e473cec788d3266
Dorian7777 <134231624+Dorian7777@users.noreply.github.com>
2023-06-05 12:47:31 +0200

    naslov

--- Code_Breaker.cpp
+++ Code_Breaker.cpp
@@ -7,7 +7,7 @@
 #include <string>
 
 using namespace std;
-
+#define NULL
 
 void igrica(char answer)
 {
@@ -19,28 +19,31 @@ void igrica(char answer)
 
 void Random()
 {
-	rand(time(NULL)); //initialize the random seed
+	rand(NULL); //initialize the random seed
 
 
 	const string  randColor[9] = { "black", "white", "red", "orange", "yellow", "pink", "blue", "green", "empty"};
-	int RandIndex = rand() % 5;
 }
 
-void diff(string diffasn)
+void diff(string diffans)
 {
-	if (diffans == "Easy")
+	if (diffans == "easy")
 	{
 		Random;
+		int RandIndex = rand() % 3;
+
 	}
 
-	else if (diffans == "Medium")
+	else if (diffans == "medium")
 	{
 		Random;
+		int RandIndex = rand() % 5;
 	}
 
-	else if (diffans == "Hard")
+	else if (diffans == "hard")
 	{
 		Random;
+		int RandIndex = rand() % 8;
 	}
 
 }
@@ -49,16 +52,23 @@ void diff(string diffasn)
 int main()
 {
 	char answer;
+	string diffans;
 	string black, white, red, orange, yellow, pink, blue, green, empty;
 	fstream datoteka;
 	cout << "Do you have what it takes to crack the code?" << endl;
 	cout << "press y/n" << endl;
 	cin >> answer;
 	igrica(answer);
+	system("cls");
+	datoteka.open("naslov.txt", ios::app | ios::in);
+	string naslov;
+	while (getline(datoteka, naslov))
+		cout << naslov << endl;
+	datoteka.close();
 	cout << "Choose a difficulty" << endl;
-	cout << "Easy\n Medium\n Hard" << endl;
+	cout << "easy\nmedium\nhard" << endl;
 	cin >> diffans;
-	diff(diffans);
+	diff(diffans);    
 	return 0;
 	
 
